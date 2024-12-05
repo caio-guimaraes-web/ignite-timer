fb9b46a272f3225e56b582393dc5b1d4c60bc7a2
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2024-12-05 11:09:13 -0300

    interrupt cycle function and disable prettier prettier plugin config on eslintconfig to stop conflict wit .vscode folder definitions

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,10 @@ import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "@typescript-eslint/eslint-plugin"
 import tsparser from "@typescript-eslint/parser"
-import rocketseatConfig from "@rocketseat/eslint-config/react.js" 
-import prettier from "eslint-plugin-prettier" 
-import prettierConfig from "eslint-config-prettier" 
-import jsxA11y from "eslint-plugin-jsx-a11y" 
+import rocketseatConfig from "@rocketseat/eslint-config/react.js"
+import prettier from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
+import jsxA11y from "eslint-plugin-jsx-a11y"
 
 export default [
   {
@@ -24,7 +24,7 @@ export default [
       react: reactHooks,
       refresh: reactRefresh,
       "@typescript-eslint": tseslint,
-      prettier, // Adicionando o Prettier aos plugins
+      /* prettier, */ // Adicionando o Prettier aos plugins
       "jsx-a11y": jsxA11y, // Adicionando o plugin JSX A11y
     },
     rules: {
